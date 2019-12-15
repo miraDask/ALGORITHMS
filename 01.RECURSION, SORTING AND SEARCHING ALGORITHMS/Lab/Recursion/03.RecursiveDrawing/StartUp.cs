da629e717df934d122103f3ff2fb12555436a25a
miraDask <mira.daskalov@gmail.com>
2019-12-15 16:37:07 +0000

    100/100

---
+++ 01.RECURSION, SORTING AND SEARCHING ALGORITHMS/Lab/Recursion/03.RecursiveDrawing/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _03.RecursiveDrawing
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            Draw(number);
+        }
+
+        private static void Draw(int number)
+        {
+            if (number == 0)
+            {
+                return;
+            }
+
+            var row = new string('*', number);
+            Console.WriteLine(row);
+            Draw(number - 1);
+            var newRow = new string('#', number);
+            Console.WriteLine(newRow);
+        }
+    }
+}
