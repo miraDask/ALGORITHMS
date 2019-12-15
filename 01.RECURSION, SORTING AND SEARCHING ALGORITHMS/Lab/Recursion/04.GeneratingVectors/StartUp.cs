2987684a2af6058cd59190ebf0b82c6fc984b433
miraDask <mira.daskalov@gmail.com>
2019-12-15 16:56:56 +0000

    100/100

---
+++ 01.RECURSION, SORTING AND SEARCHING ALGORITHMS/Lab/Recursion/04.GeneratingVectors/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _04.GeneratingVectors
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());
+            var vector = new int[number];
+
+            GenerateVectors(vector, 0);
+        }
+
+        private static void GenerateVectors(int[] vector, int index)
+        {
+            if (index > vector.Length - 1)
+            {
+                Console.WriteLine(string.Join("", vector));
+                return;
+            }
+
+            for (int i = 0; i <= 1; i++)
+            {
+                vector[index] = i;
+                GenerateVectors(vector, index + 1);
+            }
+        }
+    }
+}
