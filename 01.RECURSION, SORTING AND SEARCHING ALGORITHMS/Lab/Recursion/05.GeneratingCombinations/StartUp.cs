dd11a002524085591518f5d4b9f170570ebcb7d2
miraDask <mira.daskalov@gmail.com>
2019-12-15 17:37:45 +0000

    100/100

---
+++ 01.RECURSION, SORTING AND SEARCHING ALGORITHMS/Lab/Recursion/05.GeneratingCombinations/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.GeneratingCombinations
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var set = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var vectorLength = int.Parse(Console.ReadLine());
+
+            var vector = new int[vectorLength];
+            var index = 0;
+            int border = 0;
+
+            GenerateCombinations(set, vector, index, border);
+        }
+
+        private static void GenerateCombinations(int[] set, int[] vector, int index, int border)
+        {
+            if (index == vector.Length)
+            {
+                Console.WriteLine(string.Join(" ", vector));
+                return;
+            }
+
+            for (int i = border; i < set.Length; i++)
+            {
+                vector[index] = set[i];
+                GenerateCombinations(set, vector, index + 1, i + 1);
+            }
+        }
+    }
+}
