62df95ef29becf31a1e4427b9df3f0ab24f54802
miraDask <mira.daskalov@gmail.com>
2019-12-15 16:12:49 +0000

    initial

---
+++ 01.RECURSION, SORTING AND SEARCHING ALGORITHMS/Lab/Recursion/01.RecursiveArraySum/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.RecursiveArraySum
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(Sum(input, 0));
+        }
+
+        private static int Sum(int[] arr, int index)
+        {
+            if (index == arr.Length - 1)
+            {
+                return arr[index];
+            }
+
+            return arr[index] + Sum(arr, index + 1);
+        }
+    }
+}
