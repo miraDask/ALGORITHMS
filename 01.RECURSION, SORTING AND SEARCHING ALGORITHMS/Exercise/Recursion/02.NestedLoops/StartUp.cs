b5e458fe9ccd40deddcc0661babc1eee84b68a59
miraDask <mira.daskalov@gmail.com>
2019-12-17 12:33:55 +0000

    problem 02 Done - 100/100

---
+++ 01.RECURSION, SORTING AND SEARCHING ALGORITHMS/Exercise/Recursion/02.NestedLoops/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace _02.NestedLoops
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var vectorSize = int.Parse(Console.ReadLine());
+            var vector = new int[vectorSize];
+
+            GenerateVectors(vectorSize, 0, vector);
+        }
+
+        private static void GenerateVectors(int vectorSize, int index, int[] vector)
+        {
+            if (index == vectorSize)
+            {
+                Console.WriteLine(string.Join(" ", vector));
+                return;
+            }
+            else
+            {
+                for (int i = 1; i <= vectorSize; i++)
+                {
+                    vector[index] = i;
+                    GenerateVectors(vectorSize, index + 1, vector);
+                }
+            }
+        }
+    }
+}
