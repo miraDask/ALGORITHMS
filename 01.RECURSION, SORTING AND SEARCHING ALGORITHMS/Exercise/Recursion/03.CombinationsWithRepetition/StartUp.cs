a8ec1ba96834251ea5d0ca46667d88ccf84831e8
miraDask <mira.daskalov@gmail.com>
2019-12-17 13:47:07 +0000

    problem 03 Done - 100/100

---
+++ 01.RECURSION, SORTING AND SEARCHING ALGORITHMS/Exercise/Recursion/03.CombinationsWithRepetition/StartUp.cs
@@ -0,0 +1,37 @@
+﻿namespace _03.CombinationsWithRepetition
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numbersRange = int.Parse(Console.ReadLine());
+            var vectorSize = int.Parse(Console.ReadLine());
+
+            var vector = new int[vectorSize];
+
+            GenerateVectors(numbersRange, 0, vector, 0);
+        }
+
+        private static void GenerateVectors(int numbersRange, int index, int[] vector, int borderNumber)
+        {
+            if (index == vector.Length)
+            {
+                Console.WriteLine(string.Join(" ", vector));
+                return;
+            }
+            else
+            {
+                for (int i = 1; i <= numbersRange; i++)
+                {
+                    if (i >= borderNumber)
+                    {
+                        vector[index] = i;
+                        GenerateVectors(numbersRange, index + 1, vector, i);
+                    }
+                }
+            }
+        }
+    }
+}
