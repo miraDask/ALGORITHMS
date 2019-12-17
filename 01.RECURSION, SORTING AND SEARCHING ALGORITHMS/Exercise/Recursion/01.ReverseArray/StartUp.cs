a0b9506520c2f4844cdb3c33cf06f8568ef32aae
miraDask <mira.daskalov@gmail.com>
2019-12-17 12:19:46 +0000

    problem 01 - Done - 100/100

---
+++ 01.RECURSION, SORTING AND SEARCHING ALGORITHMS/Exercise/Recursion/01.ReverseArray/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace _01.ReverseArray
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split();
+
+            var reversedInput = ReverseArray(input, input.Length - 1);
+
+            Console.WriteLine(reversedInput);
+        }
+
+        private static string ReverseArray(string[] input, int index)
+        {
+            if (index == 0)
+            {
+                return input[0];
+            }
+
+            return input[index] + " " + ReverseArray(input, index - 1);
+        }
+    }
+}
