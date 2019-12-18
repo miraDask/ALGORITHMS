ea38a9c776e813927b1d72e2502360e17e022997
miraDask <mira.daskalov@gmail.com>
2019-12-18 21:40:42 +0000

    adding Sorting and Searching Algorithms.sln and implementation of Merge Sort

---
+++ 01.RECURSION, SORTING AND SEARCHING ALGORITHMS/Lab/Sorting and Searching Algorithms/01.MergeSort/StartUp.cs
@@ -0,0 +1,72 @@
+﻿namespace _01.MergeSort
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var unsortedArray = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Sort(unsortedArray, 0, unsortedArray.Length - 1);
+            Console.WriteLine(string.Join(" ", unsortedArray));
+        }
+
+        private static void Sort(int[] unsortedArray, int startIndex, int endIndex)
+        {
+            if (startIndex == endIndex)
+            {
+                return;
+            }
+
+            var midIndex = (startIndex + endIndex) / 2;
+
+            Sort(unsortedArray, 0, midIndex);
+            Sort(unsortedArray, midIndex + 1, endIndex);
+            Merge(unsortedArray, startIndex, endIndex, midIndex);
+        }
+
+        private static void Merge(int[] unsortedArray, int startIndex, int endIndex, int midIndex)
+        {
+            if (midIndex < 0 || midIndex + 1 >= unsortedArray.Length 
+                || unsortedArray[midIndex] <= unsortedArray[midIndex + 1])
+            {
+                return;
+            }
+
+            var helpArray = new int[unsortedArray.Length];
+
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                helpArray[i] = unsortedArray[i];
+            }
+
+            var leftIndexPointer = startIndex;
+            var rightIndexPointer = midIndex + 1;
+
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                if (leftIndexPointer > midIndex)
+                {
+                    unsortedArray[i] = helpArray[rightIndexPointer++];
+                }
+                else if (rightIndexPointer > endIndex)
+                {
+                    unsortedArray[i] = helpArray[leftIndexPointer++];
+                }
+                else if (helpArray[leftIndexPointer] <= helpArray[rightIndexPointer])
+                {
+                    unsortedArray[i] = helpArray[leftIndexPointer++];
+                }
+                else
+                {
+                    unsortedArray[i] = helpArray[rightIndexPointer++];
+                }
+            }
+        }
+    }
+}
