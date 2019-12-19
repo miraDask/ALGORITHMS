53bb9721f37c3af68bd008269daa188fe4de7c17
miraDask <mira.daskalov@gmail.com>
2019-12-19 20:16:04 +0000

    p 02

---
+++ 01.RECURSION, SORTING AND SEARCHING ALGORITHMS/Exercise/Sorting and Searching Algorithms/02.Searching/StartUp.cs
@@ -0,0 +1,46 @@
+﻿namespace _02.Searching
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var array = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var searchedElement = int.Parse(Console.ReadLine());
+
+            var indexOfSearchedElement = BinarySearch(array, searchedElement, 0, array.Length - 1);
+            Console.WriteLine(indexOfSearchedElement);
+        }
+
+        private static int BinarySearch(int[] array, int searchedElement, int startIndex, int endIndex)
+        {
+            if (endIndex < startIndex)
+            {
+                return -1;
+            }
+            else
+            {
+                var middleIndex = (startIndex + endIndex) / 2;
+
+                if (array[middleIndex] < searchedElement)
+                {
+                    return BinarySearch(array, searchedElement, middleIndex + 1, endIndex);
+                }
+                else if (array[middleIndex] > searchedElement)
+                {
+                    return BinarySearch(array, searchedElement, startIndex, middleIndex - 1);
+                }
+                else
+                {
+                    return middleIndex;
+                }
+            }
+        }
+    }
+}
