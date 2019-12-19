1f71b867c9d9194633663a2ec536066efd2d4188
miraDask <mira.daskalov@gmail.com>
2019-12-19 13:28:27 +0000

    adding problem 03 - implementing BinarySearch algorithm

--- 01.RECURSION, SORTING AND SEARCHING ALGORITHMS/Lab/Sorting and Searching Algorithms/03.BinarySearch/StartUp.cs
+++ 01.RECURSION, SORTING AND SEARCHING ALGORITHMS/Lab/Sorting and Searching Algorithms/03.BinarySearch/StartUp.cs
@@ -1,12 +1,46 @@
-﻿using System;
-
-namespace _03.BinarySearch
+﻿namespace _03.BinarySearch
 {
-    class StartUp
+    using System;
+    using System.Linq;
+
+    public class StartUp
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var array = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var searchedElement = int.Parse(Console.ReadLine());
+
+            var indexOfSearchedElement = BinarySearch(array, searchedElement, 0, array.Length - 1);
+            Console.WriteLine(indexOfSearchedElement);
+        }
+
+        private static int BinarySearch(int[] array, int searchedElement, int startIndex, int endIndex)
+        {
+            if (endIndex < startIndex)
+            {
+                return -1;
+            }
+            else
+            {
+                var middleIndex = (startIndex + endIndex) / 2;
+
+                if (array[middleIndex] < searchedElement)
+                {
+                    return BinarySearch(array, searchedElement, middleIndex + 1, endIndex);
+                }
+                else if (array[middleIndex] > searchedElement)
+                {
+                    return BinarySearch(array, searchedElement, startIndex, middleIndex - 1);
+                }
+                else
+                {
+                    return middleIndex;
+                }
+            }
         }
     }
 }
