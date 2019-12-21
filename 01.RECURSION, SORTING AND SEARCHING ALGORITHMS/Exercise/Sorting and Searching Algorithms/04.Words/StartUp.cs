900727316d11bb1e08c59fa2c2eef03b181e926b
miraDask <mira.daskalov@gmail.com>
2019-12-21 19:33:40 +0000

    adding 04 and 05 problems

---
+++ 01.RECURSION, SORTING AND SEARCHING ALGORITHMS/Exercise/Sorting and Searching Algorithms/04.Words/StartUp.cs
@@ -0,0 +1,97 @@
+﻿namespace _04.Words
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        static int count;
+        static char[] characters;
+
+        public static void Main()
+        {
+            characters = Console.ReadLine().ToCharArray();
+
+            if (CharsAreUnique())
+            {
+                count = Factorial(characters.Length);
+            }
+            else
+            {
+                GeneratePermutations(0);
+            }
+
+            Console.WriteLine(count);
+        }
+
+        private static bool CharsAreUnique()
+        {
+            var uniqueCharacters = new HashSet<char>(characters);
+
+            if (uniqueCharacters.Count == characters.Length)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static int Factorial(int number)
+        {
+            var factorial = 1;
+
+            for (int i = 2; i <= number; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+
+        private static void GeneratePermutations(int index)
+        {
+            if (index == characters.Length)
+            {
+                if (IsValid())
+                {
+                    count++;
+                }
+
+                return;
+            }
+
+            var swapped = new HashSet<char>();
+
+            for (int i = index; i < characters.Length; i++)
+            {
+                if (!swapped.Contains(characters[i]))
+                {
+                    Swap(i, index);
+                    GeneratePermutations(index + 1);
+                    swapped.Add(characters[index]);
+                    Swap(i, index);
+                }
+            }
+        }
+
+        private static bool IsValid()
+        {
+            for (int i = 0; i <= characters.Length - 2; i++)
+            {
+                if (characters[i] == characters[i + 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static void Swap(int firstIndex, int secondIndex)
+        {
+            var temp = characters[firstIndex];
+            characters[firstIndex] = characters[secondIndex];
+            characters[secondIndex] = temp;
+        }
+    }
+}
---
+++ 01.RECURSION, SORTING AND SEARCHING ALGORITHMS/Exercise/Sorting and Searching Algorithms/05.Needles/StartUp.cs
@@ -0,0 +1,77 @@
+﻿namespace _05.Needles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var countInfo = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            var needles = Console.ReadLine()
+               .Split()
+               .Select(int.Parse)
+               .ToArray();
+
+            var indecies = InsertNeedlesInNumberCollection(numbers, needles);
+            Console.WriteLine(string.Join(" ", indecies));
+        }
+
+        private static List<int> InsertNeedlesInNumberCollection(List<int> numbers, int[] needles)
+        {
+            int prev = numbers[numbers.Count - 1];
+
+            for (int i = numbers.Count - 1; i >= 0; i--)
+            {
+                if (numbers[i] == 0)
+                {
+                    numbers[i] = prev;
+                }
+
+                prev = numbers[i];
+            }
+
+            var result = new List<int>();
+            for (int i = 0; i < needles.Length; i++)
+            {
+                bool isIn = false;
+                for (int j = 0; j < numbers.Count; j++)
+                {
+                    if (numbers[j] >= needles[i])
+                    {
+                        result.Add(j);
+                        isIn = true;
+                        break;
+                    }
+                }
+
+                if (!isIn)
+                {
+                    int index = numbers.Count - 1;
+                    while (index > 0 && numbers[index] == 0)
+                    {
+                        index--;
+                    }
+                    if (numbers[index] == 0)
+                    {
+                        index--;
+                    }
+                    result.Add(index + 1);
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
