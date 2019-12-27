fa5419436303aa5186c8950f203b00855ad3ea40
miraDask <mira.daskalov@gmail.com>
2019-12-27 18:09:09 +0000

    Adding problem 03,04

---
+++ 02.COMBINATORIAL ALGORITHMS/Lab/Combinatorial Algorithms/03.VariationsWithoutRepetitions/StartUp.cs
@@ -0,0 +1,43 @@
+﻿namespace _03.VariationsWithoutRepetitions
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        static string[] arr;
+        static string[] variation;
+        static bool[] used;
+
+        public static void Main()
+        {
+            arr = Console.ReadLine().Split().ToArray();
+            var variationCount = int.Parse(Console.ReadLine());
+            variation = new string[variationCount];
+            used = new bool[arr.Length];
+
+            GenerateVariation(0);
+        }
+
+        private static void GenerateVariation(int index)
+        {
+            if (index > variation.Length - 1)
+            {
+                Console.WriteLine(string.Join(" ", variation));
+            }
+            else
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (!used[i])
+                    {
+                        used[i] = true;
+                        variation[index] = arr[i];
+                        GenerateVariation(index + 1);
+                        used[i] = false;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 02.COMBINATORIAL ALGORITHMS/Lab/Combinatorial Algorithms/04.VariationsWithRepetitions/StartUp.cs
@@ -0,0 +1,36 @@
+﻿namespace _04.VariationsWithRepetitions
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        static string[] arr;
+        static string[] variation;
+
+        public static void Main()
+        {
+            arr = Console.ReadLine().Split().ToArray();
+            var variationCount = int.Parse(Console.ReadLine());
+            variation = new string[variationCount];
+
+            GenerateVariation(0);
+        }
+
+        private static void GenerateVariation(int index)
+        {
+            if (index > variation.Length - 1)
+            {
+                Console.WriteLine(string.Join(" ", variation));
+            }
+            else
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    variation[index] = arr[i];
+                    GenerateVariation(index + 1);
+                }
+            }
+        }
+    }
+}
